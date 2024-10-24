96a57353e63d911eab812c5aa1319561cc26c591
danielbirhanu <danielbir49@gmail.com>
2024-10-24 09:45:54 +0300

    Create product model

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ import { connectdb } from './config/db.js'
 dotenv.config()
 const app = express()
 
+
+
 app.listen(process.env.PORT, () => {
     connectdb()
     console.log(`Server is running successfully at port ${process.env.PORT}`)
